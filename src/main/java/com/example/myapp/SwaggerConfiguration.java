146f2619fdf0638e2ad4586493d370aadfda51d9
yogi.singh <yogi.singh@wirecard.com>
2020-11-05 15:01:39 +0100

    product-inventory initial commit

--- src/main/java/com/example/myapp/SwaggerConfiguration.java
+++ src/main/java/com/example/myapp/SwaggerConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.myapp;
 
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +20,19 @@ SwaggerConfiguration {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.myapp.api"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                ;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("RESTFul API for managing products and categories")
+                .description("RESTFul API for managing products and categories.")
+                .version("1.0")
+                .contact(new Contact("Yogi Lal Singh", null, "yogi.lalsingh@gamil.com"))
                 .build();
     }
 }
